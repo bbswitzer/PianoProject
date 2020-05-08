38521d4a29e56f25aa6ef41a4e6c3b7a0d0ee9bd
bbswitzer <43752931+bbswitzer@users.noreply.github.com>
2020-05-08 16:21:56 -0400

    moved shift register power pin here

--- ProMicro/ProMicro.ino
+++ ProMicro/ProMicro.ino
@@ -7,15 +7,17 @@ extern const bool DEBUG_MODE = false;
 
 void setup()
 {
+	const int SHIFT_REGISTER_POWER_PIN = 19;
+	
 	intitializeRegisters();
 	delay(2000);
 
 	Serial.begin(38400);
 	Serial1.begin(38400);
 
-	pinMode(19, OUTPUT);
+	pinMode(SHIFT_REGISTER_POWER_PIN, OUTPUT);
+	digitalWrite(SHIFT_REGISTER_POWER_PIN, HIGH);
 	pinMode(LED_BUILTIN, OUTPUT);
-	digitalWrite(19, HIGH);
 
 	if(DEBUG_MODE)
 		testRegisters();
